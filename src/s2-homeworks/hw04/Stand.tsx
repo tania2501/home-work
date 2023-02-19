3a7fa0d86f5ab532cf425c5c35282efd4588f7fc
Tania <evdunova.tania@gmail.com>
2023-02-19 21:39:09 +0100

    error fixed

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -81,6 +81,7 @@ const Stand = () => {
                         id={'hw4-super-checkbox-with-text'}
                         checked={stateForAllCheckboxes}
                         onChangeChecked={setChecked}
+                        
                     >
                         some text
                     </SuperCheckbox>
@@ -90,6 +91,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
+                        onChangeChecked={setChecked}
                         onChange={(e) => setChecked(e.currentTarget.checked)
                         }
                     />
