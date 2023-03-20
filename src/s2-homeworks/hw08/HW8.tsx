89064d41acec50fd10447f76b1c36a1ae91aae5b
Tania <evdunova.tania@gmail.com>
2023-03-20 19:22:03 +0100

    hw8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -85,7 +85,7 @@ const HW8 = () => {
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
-                            <td className={s.nameCol}>Name</td>
+                            <td className={s.nameCol}>Full Name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
                         </thead>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,34 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      if (action.payload === 'up') {
+        return [
+          ...state.sort(function (a, b) {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            return 0
+          })] // need to fix
+      } else if (action.payload === 'down') {
+          return [
+            ...state.sort(function (a, b) {
+              if (a.name < b.name) return 1;
+              if (a.name > b.name) return -1;
+              return 0
+          })]
+      } else {
+        return state;
+      }
     }
+    case 'check': {
+      return state.filter(st => st.age > action.payload) // need to fix
+    }
+    default:
+      return state
+  }
 }
