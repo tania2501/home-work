4741bf210de92be494fdf4b669dc3c39dd3d4880
Tania <evdunova.tania@gmail.com>
2023-03-26 14:33:47 +0200

    fix error

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,12 +33,11 @@ function Clock() {
     
     const stringTime = date.toLocaleTimeString() // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
+    
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    let formatter = new Intl.DateTimeFormat('en-US', {weekday: 'long'})
-    let formatter2 = new Intl.DateTimeFormat('en-US', {month: 'long'})
-    const stringDay = formatter.format(date.getDay()); // пишут студенты
-    const stringMonth = formatter2.format(date.getMonth()); // пишут студенты
+   
+    const stringDay = date.toLocaleString('en-US', {weekday: 'long'}) // пишут студенты
+    const stringMonth = date.toLocaleString('en-US', {month: 'long'}) // пишут студенты
 
     return (
         <div className={s.clock}>
